912a4fa786c86070a3c8b9d5fa49d67cb454e5e1
Daniela0106 <daniela.barrientos.grisales@gmail.com>
2016-04-21 16:33:22 -0500

    Completely functional code.

--- FollowLinksOnWebsite/src/com/selenium/followlinks/FollowLinks.java
+++ FollowLinksOnWebsite/src/com/selenium/followlinks/FollowLinks.java
@@ -1,34 +1,37 @@
-//Daniela Barrientos Grisales
-
-
+/*Daniela Barrientos Grisales
+ *REST architecture:
+ *Visiting a website, finding all hyperlinks and then visiting the first one found.
+ */
 
 package com.selenium.followlinks;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.*;
-//import org.openqa.selenium.firefox.*;
 import org.openqa.selenium.By;
 import java.util.List;
 
 public class FollowLinks {
 	
 	public static int numberOfLinks = 0;
+	public static String otherUrl="";
 	
 	public static void main(String[] args){
 			    
 	    WebDriver driver = new FirefoxDriver();
-	    driver.get("http://veryinterestingis.blogspot.com.co");
+	    driver.get("http://google.com");
 	    List<WebElement> links = driver.findElements(By.tagName("a"));
 	    
 	    for(WebElement ele:links){
 	    	
 	      System.out.println(ele.getAttribute("href"));
 	      if(numberOfLinks == 0){
-	    	  driver.get(ele.getAttribute("href"));
+	    	  otherUrl = (ele.getAttribute("href"));
 	    	  numberOfLinks=1;
 	      }
 	      
 	      
 	    }
+	    
+	    driver.get(otherUrl);
 	}
 }
